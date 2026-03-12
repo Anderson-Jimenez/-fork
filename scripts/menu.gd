53d7f48b375b8ef512eb31376af7bbf6d60352fb
Anderson-Jimenez <andersonstiven.jimenez@inspineda.cat>
2026-03-12 11:58:40 +0100

    setup

---
+++ scripts/menu.gd
@@ -0,0 +1,17 @@
+extends Control
+
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/character_selection.tscn")
+
+
+func _on_settings_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_stats_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit();
